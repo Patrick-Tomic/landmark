9d49164c664d0f7bc995c68ad50e05d4c5203cb0
Patrick-Tomic <pat12tomic@gmail.com>
2025-10-20 18:46:32 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './index.css'
+function App() {
+  return(
+    <>
+    <nav className='bg-blue-500 p-4 text-white'>
+      <h1>
+        Landmark Pressure Washing and Screen Repair
+      </h1>
+    </nav>
+    </>
+  )
+}
+
+export default App
