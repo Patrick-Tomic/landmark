763be6eb4aa6661f0bcac76b85f24a27a8123b58
Patrick-Tomic <pat12tomic@gmail.com>
2025-10-22 16:04:46 -0400

    update to hero portion, first section timed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,60 @@
 import './App.css'
 import './index.css'
+import { useEffect, useState } from 'react'
 function App() {
+  const [imgASrc, setImgA] = useState('./src/assets/pressurewashA.jpeg')
+  const [imgBSrc, setImgB] = useState('./src/assets/screenrepairA.jpeg')
+  const [imgCSrc, setImgC] = useState('./src/assets/screenrepairB.jpeg')
+  const [fade, setFade] = useState(false)
+  useEffect(() => {
+    // cycle through some images
+    const images = [
+      "./src/assets/pressurewashA.jpeg",
+      "./src/assets/pressurewashB.jpeg",
+      "./src/assets/pressurewashC.jpeg",
+    ];
+    let i = 0;
+    const interval = setInterval(() => {
+      i = (i + 1) % images.length;
+      setImgA(images[i]);
+    }, 8000);
+    return () => clearInterval(interval);
+  }, []);
   return(
     <>
-    <nav className='bg-blue-500 p-4 text-white'>
+    <nav className='bg-[#1E3A8A] p-4 text-white'>
       <h1>
         Landmark Pressure Washing and Screen Repair
       </h1>
     </nav>
+    <div id="hero" className="h-screen p-24 bg-[url('./assets/tile.jpeg')] bg-repeat bg-[length:200px_200px] inset-0 ">
+    <div id="imgDiv" className="grid grid-cols-3 grid-rows-2 gap-4  h-full">
+    {/* Large main image taking 2 columns & 2 rows */}
+      <img
+        src={imgASrc}
+        alt=""
+        className={`row-span-3 col-span-2 w-[80%] h-[100%] object-cover rounded-xl transition-all duration-700 ease-in-out ${
+          fade ? "opacity-0 scale-105" : "opacity-100 scale-100"
+        }`}
+      />
+
+    {/* Right top image */}
+    <img
+      id="imgB"
+      src={imgBSrc}
+      alt=""
+      className="w-full h-full object-cover rounded-2xl"
+    />
+
+    {/* Right bottom image */}
+    <img
+      id="imgC"
+      src={imgCSrc}
+      alt=""
+      className="w-full h-full object-cover rounded-2xl"
+    />
+  </div>
+</div>
     </>
   )
 }
